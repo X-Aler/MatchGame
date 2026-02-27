510135987770d091a2210072da46aea335dac793
X_Aler <xaler836@gmail.com>
2026-02-27 16:27:40 +0300

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animeEmoji = new List<string>()
+            {
+                "💵","💵",
+                "🍄","🍄",
+                "🐦‍","🐦‍",
+                "🔥","🔥",
+                "🌑","🌑",
+                "🔆","🔆",
+                "👮","👮",
+                "🦴","🦴",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animeEmoji.Count);
+                string nextEmoji = animeEmoji[index];
+                textBlock.Text = nextEmoji;
+                animeEmoji.RemoveAt(index);
+            }
+        }
+
+        private void StartTimer()
+        {
+
+        }
+    }
+}
