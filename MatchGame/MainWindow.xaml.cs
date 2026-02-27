6533eaef96d3cfecb9cf9f1303363a50703e975b
X_Aler <xaler836@gmail.com>
2026-02-27 17:35:15 +0300

    Release (full work game)

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -8,16 +8,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        int tenthsOfSecondsElapsed;
+        int mathesFound;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            TimeTextBlock.Text = (tenthsOfSecondsElapsed / 10f).ToString("0.0s");
+
+            if (mathesFound == 8)
+            {
+                timer.Stop();
+                TimeTextBlock.Text = TimeTextBlock.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -38,16 +59,57 @@ namespace MatchGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
+                if (textBlock.Name == "TimeTextBlock") continue;
+
                 int index = random.Next(animeEmoji.Count);
+
                 string nextEmoji = animeEmoji[index];
+
                 textBlock.Text = nextEmoji;
+                textBlock.Visibility = Visibility.Visible;
+
                 animeEmoji.RemoveAt(index);
+            }
+
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            mathesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMath = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = (TextBlock)sender;
+
+            if (findingMath == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMath = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMath = false;
+                mathesFound++;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMath = false;
             }
         }
 
-        private void StartTimer()
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (mathesFound == 8)
+            {
+                SetUpGame();
+            }
         }
+
     }
 }
